7d968c4436e48ee7e5f653e8e1b42354bec2efbd
AlicornLunaa <alicornlunaa@gmail.com>
2023-10-26 14:55:18 -0500

    More testing

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,7 +1,17 @@
-void setup(){
+// Libs
 
+// Variables
+const uint8_t btnPin = 6;
+
+// Functions
+void setup() {
+    Serial.begin(115200);
+    pinMode(btnPin, INPUT_PULLUP);
 }
 
-void loop(){
-    
+void loop() {
+    if(digitalRead(btnPin) == LOW){
+        Serial.println("button1");
+        delay(500);
+    }
 }
--- web_server/src/index.js
+++ web_server/src/index.js
@@ -1,22 +1,29 @@
 const express = require("express");
+const { SerialPort } = require("serialport");
 const app = express();
 const port = 3000;
 
-app.get("/test", (req, res) => {
-    console.log("Hello world!");
-    res.send("Hello World!");
-});
+// app.get("/test", (req, res) => {
+//     console.log("Hello world!");
+//     res.send("Hello World!");
+// });
 
-app.get("/arduino", (req, res) => {
-    // arduino?cmd=""
-    console.log(req.query);
-    res.status(200).send("ok");
-});
+// app.get("/arduino", (req, res) => {
+//     // arduino?cmd=""
+//     console.log(req.query);
+//     res.status(200).send("ok");
+// });
 
-app.get("*", (req, res) => {
-    res.status(404).send("bad");
-})
+// app.get("*", (req, res) => {
+//     res.status(404).send("bad");
+// });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+// });
+
+const serialPort = new SerialPort({ path: "com8", baudRate: 115200 });
+
+serialPort.on("data", (data) => {
+    console.log(data.toString());
 });
